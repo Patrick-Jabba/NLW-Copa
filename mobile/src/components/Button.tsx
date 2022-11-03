de9c5a6c295820b7ef98c4480ba1de8b33d4d067
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-11-02 21:43:48 -0300

    componentizacao do botao com native-base interface de login

---
+++ mobile/src/components/Button.tsx
@@ -0,0 +1,33 @@
+import { Button as ButtonNativeBase, IButtonProps, Text } from "native-base";
+
+interface Props extends IButtonProps {
+  title: string;
+  type?: 'PRIMARY' | 'SECONDARY';
+}
+
+export function Button({ title,type, ...rest }: Props) {
+  return (
+    <ButtonNativeBase 
+    w="full"
+    h={14}
+    rounded="sm"
+    fontSize="md"
+    textTransform="uppercase"
+    bg={type === 'SECONDARY' ? 'red.500' : 'yellow.500'}
+    _pressed={{
+      bg: type === 'SECONDARY' ? 'red.400' : 'yellow.600'
+    }}
+    _loading={{
+      _spinner: {color: 'blue'}
+    }}
+    {...rest}
+    >
+      <Text
+      fontSize="sm"
+      fontFamily="heading"
+      color={type === 'SECONDARY' ?  'white' : 'black'}
+      
+      >{title}</Text>
+    </ButtonNativeBase>
+  );
+}
